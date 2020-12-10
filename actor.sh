03a2e424391e04ed02a4ab7a83b341a5b022b9c5
Vasiliy Sharapov <vasiliys@gmail.com>
2020-12-10 15:24:33 -0500

    Generalized browser command

--- actor.sh
+++ actor.sh
@@ -2,6 +2,9 @@
 
 set -x
 
+BROWSER_COMMAND="google-chrome"
+BROWSER_COMMAND="firefox"
+
 function randomActorNumber {
 	range=10000
 	lastFour=$RANDOM
@@ -19,12 +22,11 @@ actnumber=""
 while true; do
 	actor=""
 	actor="$(curl https://www.imdb.com/name/nm$(printf "%07d" ${actnumber})/ 2>/dev/null | grep meta.property..og.image | tr '"' '\n' | grep https | grep -v logos)"
-		if [[ "$actor" == "" ]]; then
-			actnumber="$(randomActorNumber)"
-		else
-			break
-		fi
-	done
-	google-chrome $actor 2>/dev/null
+  if [[ "$actor" == "" ]]; then
+    actnumber="$(randomActorNumber)"
+  else
+    break
+  fi
+	$BROWSER_COMMAND $actor 2>/dev/null
 done
 
